8ebd7fdeb33567598885f12c725c4afb29275116
tydino <tydino909@gmail.com>
2024-11-18 21:52:08 -0500

    camera settings
    just as it sounds, its just graphics quality (brought down to 3 levels, though i MIGHT add more) the resolution and full screen toggle, also now the settings should work properly upon build, also i fixed chikn's twisted ankle just for the sake of it!
    (just a rotaional error on my part with the ik is all :P    )

--- GOoD Night tO you/Assets/scripts/Settings/Settings.cs
+++ GOoD Night tO you/Assets/scripts/Settings/Settings.cs
@@ -2,10 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class Settings : MonoBehaviour//original by brackeys
 {
     public AudioMixer audioMixer;
+
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    void Start(){
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i=0;i<resolutions.Length;i++){
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height){
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
 
     public void SetMainVolume(float volume){
         audioMixer.SetFloat("volume", volume);
@@ -24,5 +51,18 @@ public class Settings : MonoBehaviour//original by brackeys
     }
     public void SetOtherVolume(float volume){
         audioMixer.SetFloat("volumeOther", volume);
+    }
+
+    public void SetQuality(int qualityIndex){
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetResolution(int resolutionIndex){
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetFullscreen(bool isFullscreen){
+        Screen.fullScreen = isFullscreen;
     }
 }
