d75809934db0c3f05267b6957afcc66b210d4aa1
tydino <tydino909@gmail.com>
2024-11-22 12:14:15 -0500

    Update InventoryItemData.cs
    just a comment change

--- GOoD Night tO you/Assets/scripts/inventory/InventoryItemData.cs
+++ GOoD Night tO you/Assets/scripts/inventory/InventoryItemData.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Inventory Item Data")]
-public class InventoryItemData : ScriptableObject //original by game dev guide; https://www.youtube.com/watch?v=SGz3sbZkfkg
+public class InventoryItemData : ScriptableObject //original by game dev guide; https://www.youtube.com/watch?v=SGz3sbZkfkg with editations to match what is needed for GOoD Night tO you!
 {
     public string id;
     public string displayName;
